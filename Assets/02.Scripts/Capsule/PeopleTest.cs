12ab79967105f1c23f4417faffaf8d1ec15bfb01
YY <ky040723@gmail.com>
2024-01-26 12:03:56 +0900

    캡슐화

---
+++ Assets/02.Scripts/Capsule/PeopleTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        People people = new People("이성민", 25);
+        people.SetAge(26);
+        people.SetAge(-3);
+        // people.Age = -3;  // 무결성이 깨졌다.
+        // 무결성이란 데이터의 정확성, 일관성, 유효성
+
+        Debug.Log(people.GetName());
+        Debug.Log(people.GetAge());
+    }
+
+}
